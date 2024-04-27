8a358f19044f70f2f3370c7558d5715eb41048b2
SHIVANIS-23 <168104695+SHIVANIS-23@users.noreply.github.com>
2024-04-27 21:38:48 +0530

    Add files via upload

---
+++ Pyshop/products/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Product, Offers
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+class OffersAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Offers, OffersAdmin)
---
+++ Pyshop/products/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
+
+
+class Offers(models.Model):
+    code = models.CharField(max_length=255)
+    description = models.CharField(max_length=200)
+    discount = models.FloatField()
+
+
